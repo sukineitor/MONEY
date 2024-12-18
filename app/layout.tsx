3eb1b3b757044aebbfc138f4eceeb014734ab95d
sukineitor <130964222+sukineitor@users.noreply.github.com>
2024-12-18 00:34:29 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -18,8 +19,16 @@ export default function RootLayout({
     <html lang="es">
       <body className={inter.className}>
         {children}
+        <Script
+          src="//filthygracefulspinach.com/0a/18/46/0a1846067f8ae6ca687bee688e0fdc40.js"
+          strategy="lazyOnload"
+          onError={(e) => {
+            console.error('Error loading script:', e)
+          }}
+        />
       </body>
     </html>
   )
 }
+
 
